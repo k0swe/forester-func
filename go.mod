710be662a4d16be62835a94a752b778aeac7af19
Chris Keller <xylo04@gmail.com>
2020-08-23 05:11:45 -0600

    Read public firestore data

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/xylo04/kellog-qrz-sync
 
 go 1.14
+
+require (
+	cloud.google.com/go/firestore v1.3.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible
+	google.golang.org/api v0.30.0 // indirect
+)
--- hello_http.go
+++ hello_http.go
@@ -2,24 +2,44 @@
 package kellog
 
 import (
-	"encoding/json"
+	"context"
+	firebase "firebase.google.com/go"
 	"fmt"
-	"html"
+	"log"
 	"net/http"
 )
 
-// HelloHTTP is an HTTP Cloud Function with a request parameter.
+// Called via GCP Cloud Functions
 func HelloHTTP(w http.ResponseWriter, r *http.Request) {
-	var d struct {
-		Name string `json:"name"`
-	}
-	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
-		fmt.Fprint(w, "Hello, World!")
+
+	// Use the application default credentials
+	projectID := "k0swe-kellog"
+	ctx := context.Background()
+	conf := &firebase.Config{ProjectID: projectID}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		w.WriteHeader(500)
+		_, _ = fmt.Fprintf(w, "Error initializing Firebase app: %v", err)
+		log.Printf("Error initializing Firebase app: %v", err)
 		return
 	}
-	if d.Name == "" {
-		fmt.Fprint(w, "Hello, World!")
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		w.WriteHeader(500)
+		_, _ = fmt.Fprintf(w, "Error getting Firestore client: %v", err)
+		log.Printf("Error getting Firestore client: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "Hello, %s!", html.EscapeString(d.Name))
+	defer client.Close()
+
+	docSnapshot, err := client.Collection("test").Doc("3bjCgRjsJB1BK3X1Zy8s").Get(ctx)
+	if err != nil {
+		w.WriteHeader(500)
+		_, _ = fmt.Fprintf(w, "Failed getting test data: %v", err)
+		log.Printf("Failed getting test data: %v", err)
+		return
+	}
+	m := docSnapshot.Data()
+	_, _ = fmt.Fprintf(w, "Document data: %#v\n", m)
 }
