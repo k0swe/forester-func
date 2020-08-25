8ed6b6271f51e9b1b8d865c2c235ec3f3fa89b4a
Chris Keller <xylo04@gmail.com>
2020-08-25 07:32:42 -0600

    Init clients

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/xylo04/kellog-qrz-sync
 go 1.14
 
 require (
-	cloud.google.com/go/firestore v1.3.0 // indirect
+	cloud.google.com/go/firestore v1.3.0
 	firebase.google.com/go v3.13.0+incompatible
 	google.golang.org/api v0.30.0 // indirect
 )
--- hello_http.go
+++ hello_http.go
@@ -2,37 +2,41 @@
 package kellog
 
 import (
+	"cloud.google.com/go/firestore"
 	"context"
 	firebase "firebase.google.com/go"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
-// Called via GCP Cloud Functions
-func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+// GCP_PROJECT is a user-set environment variable.
+var projectID = os.Getenv("GCP_PROJECT")
 
+// client is a global Firestore client, initialized once per instance.
+var client *firestore.Client
+
+func init() {
 	// Use the application default credentials
-	projectID := "k0swe-kellog"
 	ctx := context.Background()
 	conf := &firebase.Config{ProjectID: projectID}
 	app, err := firebase.NewApp(ctx, conf)
 	if err != nil {
-		w.WriteHeader(500)
-		_, _ = fmt.Fprintf(w, "Error initializing Firebase app: %v", err)
-		log.Printf("Error initializing Firebase app: %v", err)
+		log.Fatalf("Error initializing Firebase app: %v", err)
 		return
 	}
 
-	client, err := app.Firestore(ctx)
+	client, err = app.Firestore(ctx)
 	if err != nil {
-		w.WriteHeader(500)
-		_, _ = fmt.Fprintf(w, "Error getting Firestore client: %v", err)
-		log.Printf("Error getting Firestore client: %v", err)
+		log.Fatalf("Error getting Firestore client: %v", err)
 		return
 	}
-	defer client.Close()
+}
 
+// Hello World function. Called via GCP Cloud Functions.
+func HelloHTTP(w http.ResponseWriter, r *http.Request) {
+	ctx := context.Background()
 	docSnapshot, err := client.Collection("test").Doc("3bjCgRjsJB1BK3X1Zy8s").Get(ctx)
 	if err != nil {
 		w.WriteHeader(500)
