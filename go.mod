f637bd6db519083b78ac3294138ed15c6974ce35
Chris Keller <xylo04@gmail.com>
2020-08-25 17:15:44 -0600

    Refactor

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/xylo04/kellog-qrz-sync
 go 1.14
 
 require (
-	cloud.google.com/go/firestore v1.3.0 // indirect
+	cloud.google.com/go/firestore v1.3.0
 	firebase.google.com/go v3.13.0+incompatible
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/api v0.30.0
--- hello_http.go
+++ hello_http.go
@@ -2,7 +2,9 @@
 package kellog
 
 import (
+	"cloud.google.com/go/firestore"
 	"context"
+	"errors"
 	firebase "firebase.google.com/go"
 	"firebase.google.com/go/auth"
 	"fmt"
@@ -39,37 +41,16 @@ func init() {
 // Hello World function. Called via GCP Cloud Functions.
 func HelloHTTP(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
-	strings := r.URL.Query()["token"]
-	idToken := ""
-	if len(strings) > 0 {
-		idToken = strings[0]
-	}
-	log.Printf("token param is '%v'", idToken)
-	if idToken == "" {
-		w.WriteHeader(403)
+	idToken, err := extractIdToken(w, r)
+	if err != nil {
 		return
 	}
-	userToken, err := authClient.VerifyIDToken(ctx, idToken)
+	userToken, err := verifyToken(w, err, ctx, idToken)
 	if err != nil {
-		w.WriteHeader(500)
-		_, _ = fmt.Fprintf(w, "Failed VerifyIDTokenAndCheckRevoked: %v", err)
-		log.Printf("Failed VerifyIDTokenAndCheckRevoked: %v", err)
 		return
 	}
-	log.Printf("Token UID is %v", userToken.UID)
-	conf := &firebase.Config{ProjectID: projectID}
-	userApp, err := firebase.NewApp(ctx, conf, option.WithTokenSource(
-		oauth2.StaticTokenSource(
-			&oauth2.Token{
-				AccessToken: idToken,
-			})))
+	firestoreClient, err := makeFirestoreClient(ctx, idToken)
 	if err != nil {
-		log.Fatalf("Error initializing Firebase app: %v", err)
-		return
-	}
-	firestoreClient, err := userApp.Firestore(ctx)
-	if err != nil {
-		log.Fatalf("Error getting firestoreClient: %v", err)
 		return
 	}
 
@@ -82,4 +63,48 @@ func HelloHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 	m := docSnapshot.Data()
 	_, _ = fmt.Fprintf(w, "Document data: %#v\n", m)
+}
+
+func extractIdToken(w http.ResponseWriter, r *http.Request) (string, error) {
+	strings := r.URL.Query()["token"]
+	idToken := ""
+	if len(strings) > 0 {
+		idToken = strings[0]
+	}
+	if idToken == "" {
+		w.WriteHeader(403)
+		_, _ = fmt.Fprintf(w, "missing token")
+		return "", errors.New("missing token")
+	}
+	return idToken, nil
+}
+
+func verifyToken(w http.ResponseWriter, err error, ctx context.Context, idToken string) (*auth.Token, error) {
+	userToken, err := authClient.VerifyIDToken(ctx, idToken)
+	if err != nil {
+		w.WriteHeader(403)
+		_, _ = fmt.Fprintf(w, "Failed VerifyIDToken: %v", err)
+		log.Printf("Failed VerifyIDToken: %v", err)
+		return nil, err
+	}
+	return userToken, nil
+}
+
+func makeFirestoreClient(ctx context.Context, idToken string) (*firestore.Client, error) {
+	conf := &firebase.Config{ProjectID: projectID}
+	userApp, err := firebase.NewApp(ctx, conf, option.WithTokenSource(
+		oauth2.StaticTokenSource(
+			&oauth2.Token{
+				AccessToken: idToken,
+			})))
+	if err != nil {
+		log.Fatalf("Error initializing Firebase app: %v", err)
+		return nil, err
+	}
+	firestoreClient, err := userApp.Firestore(ctx)
+	if err != nil {
+		log.Fatalf("Error getting firestoreClient: %v", err)
+		return nil, err
+	}
+	return firestoreClient, nil
 }
