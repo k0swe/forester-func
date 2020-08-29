0fdf21edb4290d4a3bee4ceaccceb42647835bf6
Chris Keller <xylo04@gmail.com>
2020-08-29 04:33:58 -0600

    Parse contacted station fields

--- adif.go
+++ adif.go
@@ -31,30 +31,81 @@ func recordToQso(record adifparser.ADIFRecord) *adifpb.Qso {
 	qso.Band, _ = record.GetValue("band")
 	qso.BandRx, _ = record.GetValue("band_rx")
 	qso.Comment, _ = record.GetValue("comment")
-	distS, _ := record.GetValue("distance")
-	dist64, _ := strconv.ParseUint(distS, 10, 32)
-	qso.DistanceKm = uint32(dist64)
-	freq, _ := record.GetValue("freq")
-	qso.Freq, _ = strconv.ParseFloat(freq, 64)
-	freqRx, _ := record.GetValue("freq_rx")
-	qso.FreqRx, _ = strconv.ParseFloat(freqRx, 64)
+	qso.DistanceKm = getUint32(record, "distance")
+	qso.Freq = getFloat64(record, "freq")
+	qso.FreqRx = getFloat64(record, "freq_rx")
 	qso.Mode, _ = record.GetValue("mode")
 	qso.Notes, _ = record.GetValue("notes")
 	qso.PublicKey, _ = record.GetValue("public_key")
 	qso.Complete, _ = record.GetValue("qso_complete")
 	qso.TimeOn = getTimestamp(record, "qso_date", "time_on")
 	qso.TimeOff = getTimestamp(record, "qso_date_off", "time_off")
-	random, _ := record.GetValue("random")
-	qso.Random = random == "Y"
+	qso.Random = getBool(record, "random")
 	qso.RstReceived, _ = record.GetValue("rst_rcvd")
 	qso.RstSent, _ = record.GetValue("rst_sent")
 	qso.Submode, _ = record.GetValue("submode")
-	swl, _ := record.GetValue("swl")
-	qso.Swl = swl == "Y"
+	qso.Swl = getBool(record, "swl")
+
+	// TODO: where to put this?
+	_, _ = record.GetValue("app_qrzlog_logid")
 
 	qso.ContactedStation = new(adifpb.Station)
-	qso.ContactedStation.OpCall, _ = record.GetValue("call")
+	qso.ContactedStation.Address, _ = record.GetValue("address")
+	qso.ContactedStation.Age = getUint32(record, "age")
+	qso.ContactedStation.StationCall, _ = record.GetValue("call")
+	qso.ContactedStation.County, _ = record.GetValue("cnty")
+	qso.ContactedStation.Continent, _ = record.GetValue("cont")
+	qso.ContactedStation.OpCall, _ = record.GetValue("contacted_op")
+	qso.ContactedStation.Country, _ = record.GetValue("country")
+	qso.ContactedStation.CqZone = getUint32(record, "cqz")
+	qso.ContactedStation.DarcDok, _ = record.GetValue("darc_dok")
+	qso.ContactedStation.Dxcc = getUint32(record, "dxcc")
+	qso.ContactedStation.Email, _ = record.GetValue("email")
+	qso.ContactedStation.OwnerCall, _ = record.GetValue("eq_call")
+	qso.ContactedStation.Fists = getUint32(record, "fists")
+	qso.ContactedStation.FistsCc = getUint32(record, "fists_cc")
+	qso.ContactedStation.GridSquare, _ = record.GetValue("gridsquare")
+	qso.ContactedStation.Iota, _ = record.GetValue("iota")
+	qso.ContactedStation.IotaIslandId = getUint32(record, "iota_island_id")
+	qso.ContactedStation.ItuZone = getUint32(record, "ituz")
+	qso.ContactedStation.Latitude = getFloat64(record, "lat")
+	qso.ContactedStation.Longitude = getFloat64(record, "lon")
+	qso.ContactedStation.OpName, _ = record.GetValue("name")
+	qso.ContactedStation.Pfx, _ = record.GetValue("pfx")
+	qso.ContactedStation.QslVia, _ = record.GetValue("qsl_via")
+	qso.ContactedStation.City, _ = record.GetValue("qth")
+	qso.ContactedStation.Region, _ = record.GetValue("region")
+	qso.ContactedStation.Rig, _ = record.GetValue("rig")
+	qso.ContactedStation.Power = getFloat64(record, "rx_pwr")
+	qso.ContactedStation.Sig, _ = record.GetValue("sig")
+	qso.ContactedStation.SigInfo, _ = record.GetValue("sig_info")
+	qso.ContactedStation.SilentKey = getBool(record, "silent_key")
+	qso.ContactedStation.Skcc, _ = record.GetValue("skcc")
+	qso.ContactedStation.SotaRef, _ = record.GetValue("sota_ref")
+	qso.ContactedStation.State, _ = record.GetValue("state")
+	qso.ContactedStation.TenTen = getUint32(record, "ten_ten")
+	qso.ContactedStation.Uksmg = getUint32(record, "uksmg")
+	qso.ContactedStation.UsacaCounties, _ = record.GetValue("usaca_counties")
+	qso.ContactedStation.VuccGrids, _ = record.GetValue("vucc_grids")
+	qso.ContactedStation.Web, _ = record.GetValue("web")
 	return qso
+}
+
+func getBool(record adifparser.ADIFRecord, field string) bool {
+	st, _ := record.GetValue(field)
+	return st == "Y"
+}
+
+func getFloat64(record adifparser.ADIFRecord, field string) float64 {
+	st, _ := record.GetValue(field)
+	fl, _ := strconv.ParseFloat(st, 64)
+	return fl
+}
+
+func getUint32(record adifparser.ADIFRecord, field string) uint32 {
+	s, _ := record.GetValue(field)
+	i64, _ := strconv.ParseUint(s, 10, 32)
+	return uint32(i64)
 }
 
 func getTimestamp(record adifparser.ADIFRecord, dateField string, timeField string) *timestamp.Timestamp {
